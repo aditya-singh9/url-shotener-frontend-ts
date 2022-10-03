65f9d1bbcbd6e9a5ea832a2d2d63a11cd9ff4416
Soustab Haldar <soustabhaldar10@gmail.com>
2022-10-03 13:01:14 +0530

    added clearbtn

--- src/components/URLShortenerForm.tsx
+++ src/components/URLShortenerForm.tsx
@@ -3,14 +3,14 @@ import { useState, useRef, useEffect, CSSProperties } from "react";
 import { SERVER_ENDPOINTS } from "../config";
 import "./URLShortenerForm.css";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import ClearIcon from "@mui/icons-material/Clear";
 import CopyToClipboard from "react-copy-to-clipboard";
 import toast, { Toaster } from "react-hot-toast";
 import PropagateLoader from "react-spinners/PropagateLoader";
 
-
 function URLShortenerForm() {
-  const [destination, setDestination] = useState();
+  const [destination, setDestination] = useState("");
   const divRef = useRef<HTMLAnchorElement>(null);
   const [loading, setloading] = useState(true);
   const [forks, setforks] = useState(0);
@@ -37,17 +37,22 @@ function URLShortenerForm() {
   const text = () => {
     toast.success("Copied!");
   };
-
+  const clearText = () => {
+    console.log("clearing...");
+    setDestination("");
+  };
 
   const getForksStarsCount = async () => {
-    const { data } = await axios.get(`https://api.github.com/repos/aditya-singh9/url-shotener-frontend-ts`);
+    const { data } = await axios.get(
+      `https://api.github.com/repos/aditya-singh9/url-shotener-frontend-ts`
+    );
     setforks(data.forks_count);
     setstars(data.stargazers_count);
     setloading(false);
   };
   useEffect(() => {
     getForksStarsCount();
-  }, [])
+  }, []);
 
   const override: CSSProperties = {
     display: "block",
@@ -58,14 +63,12 @@ function URLShortenerForm() {
   };
 
   if (!loading) {
-
     return (
       <>
         <div className="outer">
           <div>
             <Toaster />
           </div>
-
 
           <div className="head-div">
             <p className="head">
@@ -74,10 +77,16 @@ function URLShortenerForm() {
           </div>
           <div className="forks_stars_div">
             <div className="block_div">
-              <a className="btn" href="https://github.com/aditya-singh9/url-shotener-frontend-ts" rel="noreferrer noopener" target="_blank" aria-label="Fork aditya-singh9/kekfinder on GitHub">
+              <a
+                className="btn"
+                href="https://github.com/aditya-singh9/url-shotener-frontend-ts"
+                rel="noreferrer noopener"
+                target="_blank"
+                aria-label="Fork aditya-singh9/kekfinder on GitHub"
+              >
                 <div className="block_divLeft">
-
-                  <StarBorderIcon className="starbtn" /><p className="forks_stars_text">Star</p>
+                  <StarBorderIcon className="starbtn" />
+                  <p className="forks_stars_text">Star</p>
                 </div>
               </a>
               <div className="black_pipe"> </div>
@@ -86,15 +95,28 @@ function URLShortenerForm() {
               </div>
             </div>
             <div className="block_div">
-              <a className="btn" href="https://github.com/aditya-singh9/url-shotener-frontend-ts/fork" rel="noreferrer noopener" target="_blank" aria-label="Fork aditya-singh9/kekfinder on GitHub">
+              <a
+                className="btn"
+                href="https://github.com/aditya-singh9/url-shotener-frontend-ts/fork"
+                rel="noreferrer noopener"
+                target="_blank"
+                aria-label="Fork aditya-singh9/kekfinder on GitHub"
+              >
                 <div className="block_divRight">
-
-                  <svg className="fork_logo" viewBox="0 0 16 16" width="20" height="20" margin-top="2" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z">
-                    </path>
+                  <svg
+                    className="fork_logo"
+                    viewBox="0 0 16 16"
+                    width="20"
+                    height="20"
+                    margin-top="2"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                    ></path>
                   </svg>
                   <p className="fork">Fork</p>
-
                 </div>
               </a>
               <div className="black_pipe"> </div>
@@ -102,15 +124,19 @@ function URLShortenerForm() {
                 <p>{forks}</p>
               </div>
             </div>
-
           </div>
           <div className="outer">
             <form className="form" onSubmit={handleSubmit}>
               <input
                 className="input"
+                id="input"
                 placeholder="Enter a URL"
                 onChange={(e: any) => setDestination(e.target.value)}
+                value={destination}
               />
+              <div className="clearBtn" onClick={clearText}>
+                <ClearIcon></ClearIcon>
+              </div>
               <button type="submit" className="button">
                 Create!
               </button>
@@ -152,11 +178,15 @@ function URLShortenerForm() {
       </>
     );
   } else {
-    return ( 
-       <PropagateLoader color="#0070f3" loading={loading} cssOverride={override} size={15} />
+    return (
+      <PropagateLoader
+        color="#0070f3"
+        loading={loading}
+        cssOverride={override}
+        size={15}
+      />
     );
   }
-
 }
 
 export default URLShortenerForm;
